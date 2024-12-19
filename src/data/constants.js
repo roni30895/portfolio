f8e1b78c2a3c1a93d544efeab66068e0bc2ad436
Rohan Nanabhau Pawar <106943166+roni30895@users.noreply.github.com>
2024-12-19 17:48:54 +0530

    Rohan Portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,362 @@
+export const Bio = {
+  name: "Rohan Pawar",
+  roles: [
+    "Python Developer",
+    "AI Engineer",
+    "Go Developer",
+  ],
+  description:
+    "Experienced Python Developer specializing in cloud-native solutions, microservices architecture, and RESTful APIs. Skilled in IBM Cloud, Docker, Kubernetes, and CI/CD pipelines for scalable and secure application development. Expertise in data science, machine learning, and deep learning technologies using tools like NumPy, Pandas, and TensorFlow. Passionate about creating efficient, high-performance solutions with a focus on Infrastructure as Code (IaC), DevOps, and innovative automation. Check out my repositories for projects showcasing microservices, containerization, AI-driven solutions, and advanced data visualization using tools like Matplotlib and Seaborn.",
+  github: "https://github.com/roni30895",
+  resume:
+    "https://drive.google.com/file/d/1YwFf6WwxNlS268GzTJI8qHAP4Qm2dR3w/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/rohan-pawar-563ab5125/",
+  email:"rohanpawar628@gmail.com"
+};
+
+export const skills = [
+  {
+    title: "Programming Languages & Frameworks",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
+      },
+      {
+        name: "Golang",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/go/go-original.svg",
+      },
+      {
+        name: "Flask",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNqtHYopo_XXmaa60pJtj_7yNPnYsWU_KkZmtNyHxkPA&s",
+      },
+      {
+        name: "Django",
+        image:
+          "https://image.spreadshirtmedia.com/image-server/v1/mp/products/T1459A839PA3861PT28D1007202637W10000h5483/views/1,width=800,height=800,appearanceId=839,backgroundColor=F2F2F2/django-logo-sticker.jpg",
+      },
+      {
+        name: "Gin",
+        image:
+          "https://raw.githubusercontent.com/gin-gonic/logo/master/color.png"
+      },
+      {
+        name: "Ginkgo",
+        image:
+          "https://onsi.github.io/ginkgo/images/ginkgo.png"
+        },
+    ],
+  },
+  {
+    title: "Artificial Intelligence & Data Science",
+    skills: [
+      {
+        name: "Machine Learning",
+        image:
+        "https://w7.pngwing.com/pngs/326/911/png-transparent-deep-learning-with-keras-machine-learning-artificial-intelligence-others-angle-white-text-thumbnail.png",
+      },
+      {
+        name: "Deep Learning",
+        image: "https://cdn-icons-png.flaticon.com/512/2103/2103718.png"
+      },
+      {
+        name: "NLP",
+        image:
+          "https://miro.medium.com/v2/resize:fit:1184/0*zKRz1UgqpOZ4bvuA",
+      },
+
+      {
+        name: "Large Language Models",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/1/13/ChatGPT-Logo.png",
+      },
+      {
+        name: "Computer Vision",
+        image:
+          "https://static-00.iconduck.com/assets.00/opencv-icon-1657x2048-3wu3ib6x.png",
+      },
+      {
+        name: "Generative AI",
+        image: "https://ih1.redbubble.net/image.4911784195.8379/st,small,507x507-pad,600x600,f8f8f8.jpg",
+      },
+      {
+        name: "Prompt Engineering",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYsHf6S3lBIP9ULSpX5f9-fbLgscLe7y_k7bd3yCt_t9641fKW0rkbhU1ZjAv-DctWGT0&usqp=CAU",
+      },
+    ],
+  },
+  {
+    title: "Artificial Intelligence & Data Science",
+    skills: [
+      {
+        name: "Scikit-learn",
+        image:
+        "https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg",
+      },
+      {
+        name: "TensorFlow",
+        image: "https://www.vectorlogo.zone/logos/tensorflow/tensorflow-icon.svg"
+      },
+      {
+        name: "PyTorch",
+        image:
+          "https://www.vectorlogo.zone/logos/pytorch/pytorch-icon.svg"
+      },
+      {
+        name: "Numpy",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvEfXHLIJLACY1DI30rAzDlbekLkgFXYm1DA&s"
+      },
+      {
+        name: "Pandas",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/pandas/pandas-original.svg"
+      },
+      {
+        name: "Langchain",
+        image:
+          "https://avatars.githubusercontent.com/u/126733545?s=280&v=4"
+      },
+      {
+        name: "ollama",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkYYSwi-WiW9rVrDT4iEJC_Rfpii57iFBf0g&s",
+      },
+      {
+        name: "Llamaindex",
+        image:
+          "https://www.devturtleblog.com/wp-content/uploads/2024/05/llamaindex-wallpaper.webp",
+      },
+    ],
+  },
+  {
+    title: "Cloud",
+    skills: [
+      {
+        name: "IBM Cloud",
+        image:
+          "https://logos-world.net/wp-content/uploads/2020/09/IBM-Symbol.jpg",
+      },
+      {
+        name: "Azure",
+        image:
+          "https://www.vectorlogo.zone/logos/microsoft_azure/microsoft_azure-icon.svg",
+      },
+      {
+        name: "Heroku",
+        image:
+          "https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "DevOps and Development Tools ",
+    skills: [
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Kubernetes",
+        image:
+          "https://www.svgrepo.com/show/376331/kubernetes.svg",      
+      },
+      {
+        name: "Jenkins CI/CD pipelines",
+        image:
+          "https://www.vectorlogo.zone/logos/jenkins/jenkins-icon.svg"
+        },
+      {
+        name: "Tekton",
+        image:
+          "https://avatars.githubusercontent.com/u/47602533?s=280&v=4",
+      },
+      {
+        name: "Git",
+        image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1200px-Git_icon.svg.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://img.icons8.com/?size=48&id=0OQR1FYCuA9f&format=png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://www.svgrepo.com/show/354202/postman-icon.svg",
+      },
+      {
+        name: "Microservices",
+        image:
+          "https://runestone.academy/ns/books/published/py4e-int/_images/web_crawler.png",
+      },
+    ],
+  },
+  {
+    title: "Data Analytics & Visualization",
+    skills: [
+      {
+        name: "Data Analysis",
+        image: "https://w7.pngwing.com/pngs/73/309/png-transparent-yellow-graph-increase-icon-or-logo.png",
+      },
+      {
+        name: "SQL",
+        image:
+          "https://symbols.getvecta.com/stencil_28/61_sql-database-generic.90b41636a8.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://cdn-icons-png.flaticon.com/512/919/919836.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://w7.pngwing.com/pngs/956/695/png-transparent-mongodb-original-wordmark-logo-icon-thumbnail.png",
+      },
+      {
+        name: "Oracle",
+        image:
+          "https://media.licdn.com/dms/image/D5612AQGTzGGnu886Vw/article-cover_image-shrink_720_1280/0/1680581578662?e=2147483647&v=beta&t=Jc3XYEXXsZNfua5Dy96AX17G59H2LrEj4mcGb6rWt60",
+      },
+      {
+        name: "Plotly",
+        image:
+          "https://avatars.githubusercontent.com/u/5997976?s=280&v=4"
+      },
+      {
+          name: "Seaborn",
+          image:
+            "https://seaborn.pydata.org/_images/logo-mark-lightbg.svg"
+      },
+      {
+        name: "Tableau",
+        image:
+          "https://w7.pngwing.com/pngs/815/646/png-transparent-tableau-full-logo-tech-companies-thumbnail.png"
+        },
+      {
+        name: "Power BI",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlW7tjWKlsaJgq5zgUEdhTQBJJ0bi2bBhq1g&s"
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://logos-world.net/wp-content/uploads/2020/09/IBM-Symbol.jpg",
+    role: "Python Developer",
+    company: "IBM Cloud, Bengaluru, India",
+    date: "Dec 2022 - Dec 2023",
+    desc: `Project Title: <strong>IaaS Compliance Automation Datalake & IBM Security and Compliance Center (SCC)</strong>
+
+    <h5>Objective:</h5>
+    <p>Automate security compliance checks for applications hosted on IBM Cloud, ensuring adherence to industry standards and achieving a 100% audit success rate.</p>
+    
+    <h5>Responsibilities:</h5>
+    <ul>
+      <li>Developed cloud-native microservices in Python to streamline data collection and automate compliance reporting.</li>
+      <li>Automated project management reporting using JIRA RESTful APIs, reducing report generation time by 90%.</li>
+      <li>Optimized infrastructure costs by 45% through containerized solutions with Docker and Kubernetes.</li>
+      <li>Migrated Python-based solutions to Go microservices, boosting system efficiency by 60%.</li>
+      <li>Collaborated with cross-functional teams to identify and resolve application issues, ensuring data security and integrity.</li>
+    </ul>
+    
+    <h5>Technologies Used:</h5>
+    <ul>
+      <li>Languages: Python, Golang</li>
+      <li>Frameworks/Tools: Docker, Kubernetes, JIRA RESTful APIs, CI/CD Pipelines</li>
+      <li>Cloud Services: IBM Cloud Object Storage, IBM Log Analysis with LogDNA</li>
+      <li>Version Control: Git, GitHub</li>
+    </ul>
+    
+    <h5>Achievements:</h5>
+    <ul>
+      <li>Reduced manual efforts for compliance reporting by 85% through automation.</li>
+      <li>Improved server provisioning efficiency by 20% and reduced operational costs significantly.</li>
+    </ul>
+    `,
+    skills: [
+      "Python",
+      "Golang",
+      "Docker",
+      "Kubernetes",
+      "JIRA RESTful APIs",
+      "CI/CD Pipelines",
+      "IBM Cloud",
+      "IBM COS",
+      "Automation",
+      "Cloud-Native Solutions"
+    ],
+    doc: "",
+  }
+];
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://cdac.in/img/C-DAC.jpg",
+    school: "Centre for Development of Advanced Computing (C-DAC), Noida",
+    date: "Mar 2022 - Sep 2022",
+    grade: "Distinction",
+    desc: `I hold a Post Graduate Diploma in Artificial Intelligence from CDAC Noida, where I excelled with distinction.<br>
+    Throughout the program, I gained expertise in Python, data analytics, machine learning, deep learning, natural language processing, cloud computing, Docker, Kubernetes, statistics, and Jenkins for CI/CD.<br>
+    Hands-on projects and collaborative efforts refined my problem-solving abilities and communication skills.<br>
+    I'm passionate about leveraging AI to innovate and tackle real-world challenges.<br>`,
+    degree: "PG Diploma in Artificial Intelligence",
+  },
+  {
+    id: 1,
+    img: "https://dbatu.ac.in/wp-content/uploads/2022/12/cropped-OQSD-Wtj.jpeg",
+    school: "Rajiv Gandhi Proudyogiki Vishwavidyalaya",
+    date: "Jun 2018 - Jun 2020",
+    grade: "A",
+    desc: "I am a distinguished post graduate holding a Master of Technology from Dr.Babasaheb Ambedkar Technological University with specialisation in Machine Design.",
+    degree: "Master of Technology",
+  },
+];
+
+export const projects = [
+
+  {
+    id: 0,
+    title: "Tumor Detector (Brain Tumor Classification Web Application) | CDAC | Sept 2022",
+    date: "Sept 2022",
+    description:
+    "Developed a system utilizing Deep Learning to detect and classify brain tumors from MRI images. The project employed Convolutional Neural Networks (CNNs) to identify tumor blocks and classify tumor types efficiently. This system aimed to enhance diagnostic accuracy and support healthcare professionals in timely and informed treatment decisions. The project demonstrates expertise in AI/ML, Computer Vision, and healthcare innovation.",
+    image:
+      "https://i.gifer.com/O0Wu.gif",
+    tags: ["Python","Machine Learning"," Deep Learning", "CNN", "Keras", "Transfer Learning", "Flask", "TensorFlow", "scikit-learn", "Heroku","Pandas","Numpy"],
+    category: "Artificial Intaligence",
+    github: "https://github.com/roni30895/TumorDetector",
+    webapp: "https://github.com/roni30895/TumorDetector",
+  },
+  {
+    id: 1,
+    title: "MediQuick Cure - Medical Appointment Booking System",
+    date: "March 2023",
+    description:
+      "Developed a comprehensive medical appointment booking system aimed at simplifying healthcare services. MediQuick Cure enables users to book appointments with various medical professionals (e.g., doctors, pathologists, nurses), while also offering medical professionals a seamless environment to manage and schedule their appointments.",
+    image:
+      "https://simplybook.me/build/images/trial-section/medical.7358dc7d.png",
+    tags: [
+      " Golang", "Gin (HTTP Web Framework)", "MySQL","MongoDB","Postman", "Git","GitHub", "VS Code",
+    ],
+    category: "web app",
+    github: "https://github.com/roni30895/MediQuickCure",
+    webapp: "https://github.com/roni30895/MediQuickCure",
+  },
+
+];
---
+++ src/utils/Themes.js
@@ -0,0 +1,23 @@
+export const darkTheme = {
+    bg:"#FFFFFF",
+    bgLight: "#f0f0f0",
+    primary:"#be1adb",
+    text_primary:"#111111",
+    text_nav:"#F2F3F4",
+    text_secondary:"#48494a",
+    card:"#FFFFFF",
+    card_light: '#191924',
+    button:"#5c5b5b",
+    white:"#FFFFFF",
+    black:"#000000",
+};
+
+export const lightTheme = {
+    bg: "#F8F9FA",
+    bgLight: "#EDEFF1",
+    primary: "#007BFF",
+    text_primary: "#212529",
+    text_secondary: "#6C757D",
+    card: "#FFFFFF",
+    button: "#17A2B8",
+};
